ec57ecc42c92b7163c183ca8e815ee463af178c2
Your Name <deepuisme.rd@gmail.com>
2021-11-25 11:18:25 +0545

    First Commit

---
+++ src/ifelse/Output.java
@@ -0,0 +1,8 @@
+package ifelse;
+
+public class Output {
+    public static void main(String[] args) {
+        control call = new control();
+        call.print();
+    }
+}
---
+++ src/ifelse/control.java
@@ -0,0 +1,9 @@
+package ifelse;
+
+public class control {
+    public void print(){
+        int a = 5;
+        int result = ( a  > 100) ?200:300;
+        System.out.println(result);
+    }
+}
