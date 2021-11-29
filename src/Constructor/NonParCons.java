889fd93cb88b02b63ddd4beb2b64f534ab31f6aa
Your Name <deepuisme.rd@gmail.com>
2021-11-29 08:36:20 +0545

    Nov 29 Class Time

---
+++ src/Constructor/NonParCons.java
@@ -0,0 +1,13 @@
+package Constructor;
+
+public class NonParCons {
+
+    //Constructor
+    public NonParCons(){
+        System.out.println("This ia No Parameterized Constructor.");
+    }
+
+    public static void main(String[] args) {
+        NonParCons nonParCons = new NonParCons();
+    }
+}
---
+++ src/Constructor/ParaCons.java
@@ -0,0 +1,19 @@
+package Constructor;
+
+public class ParaCons {
+    //Instance var
+    public int id;
+    public String name;
+
+    //Constructor
+    public ParaCons(int id, String name) {
+        this.id = id;
+        this.name = name;
+        System.out.println("Id is: " + id + " " + "Name is: " + name);
+
+    }
+
+    public static void main(String[] args) {
+        ParaCons paraCons = new ParaCons(121,"java");
+    }
+}
